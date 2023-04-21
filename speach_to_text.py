9c1bec88fe56445ae9b65dbd5edde3ac417e8df4
kohei110 <kohei.nishitani@gmail.com>
2023-04-22 08:57:42 +0900

    audio wip

--- speach_to_text.py
+++ speach_to_text.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv
 import sys
 
 load_dotenv() 
-api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 file_path = sys.argv[1]
-print(file_path)
+audio_file = open(file_path, "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
+print(transcript.text)
