5a3af7c17cb8e4379179b47bc7609dea21af5955
kohei110 <kohei.nishitani@gmail.com>
2023-04-22 08:47:49 +0900

    audio

--- speach_to_text.py
+++ speach_to_text.py
@@ -1,8 +1,10 @@
 import openai
 import os
 from dotenv import load_dotenv
+import sys
 
-load_dotenv()  # Load variables from .env file
-
+load_dotenv() 
 api_key = os.getenv('OPENAI_API_KEY')
-print(api_key)
+
+file_path = sys.argv[1]
+print(file_path)
