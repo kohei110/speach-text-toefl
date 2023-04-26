9c8a82390aa763640bd1c1bc66ffb0bf7ff6ebb1
kohei110 <kohei.nishitani@gmail.com>
2023-04-26 20:58:02 +0900

    small update

--- speach_to_text.py
+++ speach_to_text.py
@@ -9,4 +9,6 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 file_path = sys.argv[1]
 audio_file = open(file_path, "rb")
 transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+print("Whisper API Output")
 print(transcript.text)
